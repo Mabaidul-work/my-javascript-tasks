65fbb9e89759e1f079754bf349d73b337fd37682
Mabaidul-work <mabaidul@appscrip.co>
2024-10-07 21:09:55 +0530

    first commit

---
+++ src/constants/data.js
@@ -0,0 +1,13 @@
+export const myTasks = [
+  {
+    id: 1,
+    name: "Piant App By Canvas Api",
+    description: "This is the javaScript task 1",
+    gitHubLink: "https://github.com/Mabaidul-work/paint-app",
+    hostedLink: "https://paint-app-opal.vercel.app/",
+    completed: true,
+  },
+  { id: 2, name: "Task 2", gitHubLink: "", hostedLink: "", completed: false },
+  { id: 3, name: "Task 3", gitHubLink: "", hostedLink: "", completed: false },
+  { id: 4, name: "Task 4", gitHubLink: "", hostedLink: "", completed: false },
+];
